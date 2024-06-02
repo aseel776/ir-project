cb468ca01ae855d9688a32817828ccb8a333bc34
Aseel <aseeldibi@gmail.com>
2024-06-02 20:32:25 +0300

    precision and recall

---
+++ src/evaluation/recall.py
@@ -0,0 +1,12 @@
+def get_recall(retrieved_ids, relevant_ids):
+
+    if not relevant_ids:
+        return 0.0
+    
+    # get Tp
+    relevant_and_retrieved = [id for id in retrieved_ids if id in relevant_ids]
+
+    # recall = Tp / (Tp + Fn)
+    recall = len(relevant_and_retrieved) / len(relevant_ids)
+    
+    return recall
