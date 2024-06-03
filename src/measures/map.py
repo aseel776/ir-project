50d51156a53cf36a8b0fa46eecba094d58db7627
Aseel <aseeldibi@gmail.com>
2024-06-03 05:19:01 +0300

    added get_ap function and implement it in dataset2 evaluation

--- src/measures/map.py
+++ src/measures/map.py
@@ -23,6 +23,24 @@ def get_ap_at_k(retrieved_ids, relevant_ids, k):
 
     return ap
 
+def get_ap(retrieved_ids, relevant_ids):
+
+    relevant_and_retrieved = 0
+    
+    precision_sum = 0
+
+    for i, id in enumerate(retrieved_ids, start=1):
+        if retrieved_ids[i - 1] in relevant_ids:
+            relevant_and_retrieved += 1
+            precision_sum += get_precision_at_k(retrieved_ids, relevant_ids, i)
+    
+    if relevant_and_retrieved == 0:
+        return 0.0
+    
+    ap = precision_sum / relevant_and_retrieved
+
+    return ap
+
 def get_map(aps: list):
     if not aps:
         return 0.0
