291238a3d8fe61f48823ae07a71f1819d4358707
Aseel <aseeldibi@gmail.com>
2024-06-03 13:41:02 +0300

    avg precision added

--- src/measures/precision.py
+++ src/measures/precision.py
@@ -2,15 +2,16 @@ def get_precision_at_k(retrieved_ids, relevant_ids, k):
 
     # get top k retrieved
     retrieved_at_k = retrieved_ids[:k]
-    print(f'Retrieved at {k}')
-    print(retrieved_at_k)
     
     # get (Tp in k)
     relevant_and_retrieved = [id for id in retrieved_at_k if id in relevant_ids]
-    print('Relevant and Retrieved')
-    print(relevant_and_retrieved)
     
     # P = (Tp in k) / k
     precision = len(relevant_and_retrieved) / k
     
     return precision
+
+def get_avg_precision(precisions: list):
+    if not precisions:
+        return 0.0
+    return sum(precisions) / len(precisions)
